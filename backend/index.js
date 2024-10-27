0b6a99f237408c3e76ed19f07d3aef555bc890ab
sabujsarker382 <75823580+sabujsarker382@users.noreply.github.com>
2024-10-28 04:54:29 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,22 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
-
-
 const app = express()
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://ecommerec-website-front-hb2rhnhgi-sabuj-sarkers-projects.vercel.app'
+];
+
+
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json())
 app.use(cookieParser())
 
